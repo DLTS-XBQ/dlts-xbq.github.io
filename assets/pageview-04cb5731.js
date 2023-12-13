5975d11c0332074809b8af751a76607dd59d61de
baiqing.xie <baiqing.xie@swiftpass.cn>
2023-12-13 19:21:31 +0800

    deploy

--- assets/pageview-04cb5731.js
+++
@@ -1,1 +0,0 @@
-import{aa as e}from"./app-dc210946.js";const a=()=>e({serverURL:"https://waline-git-main-dlts-xbqs-projects.vercel.app/"});export{a as updatePageview};
---
+++ assets/pageview-867e982d.js
@@ -0,0 +1,1 @@
+import{aa as e}from"./app-c3f53c31.js";const a=()=>e({serverURL:"https://waline-git-main-dlts-xbqs-projects.vercel.app/"});export{a as updatePageview};
